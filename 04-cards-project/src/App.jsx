cddcefbadfcdad23f70525b91f4310ea8b848be0
Ukeshwarninawe <ukeshwargulabninawe@gmail.com>
2026-06-21 20:33:43 +0530

    Tailwind CSS

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+
+      <div className="card">
+        <div className="top">
+          <img src="https://images.seeklogo.com/logo-png/40/2/amazon-icon-logo-png_seeklogo-405254.png" alt="" />
+          <button>Save<Bookmark size={12}/></button>
+        </div>
+        <div className="center">
+          <h3>Amazon <span>5 days ago</span></h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div>
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+              <h3>$120/hr</h3>
+              <p>Mumbar, India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
