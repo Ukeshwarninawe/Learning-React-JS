4b6f2fb78a1c740e0833e70dc468710db287470d
Ukeshwarninawe <ukeshwargulabninawe@gmail.com>
2026-06-23 17:05:40 +0530

    UI Project Done using ReactJS Topics: Components, Props and Tailwind CSS

---
+++ 07-ui-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-6'>
+        <h3 className='text-7xl font-bold mb-7 leading-[1.1]'>Prospective<br /><span>customer</span><br/>segmentation</h3>
+        <p className='text-xl font-medium text-gray-600 rounded-full px-3 py-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, laboriosam. Facere aut dolores dolore consequatur pariatur necessitatibus alias laboriosam cum?</p>
+    </div>
+  )
+}
+
+export default HeroText
