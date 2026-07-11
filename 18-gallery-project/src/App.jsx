0273af1fa28870c9a9a0166075449c48b58edc41
Ukeshwarninawe <ukeshwargulabninawe@gmail.com>
2026-07-11 12:04:08 +0530

    Gallery Appilication

---
+++ 18-gallery-project/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Card from './components/Card'
+const App = () => {
+
+  const [userData, setUserData] = useState([]);
+
+  const [index, setIndex] = useState(1)
+
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+    setUserData(response.data);
+    console.log(response.data);
+    
+  }
+
+  useEffect(function(){
+    getData()
+  },[index])
+
+  let printUserData = <h3 className='text-gray-300 text-xs font-semibold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Loading...</h3>
+  if(userData.length>0){
+    printUserData = userData.map(function(elem,idx){
+      return <div key={idx}>
+        <Card elem={elem}/>
+      </div>
+    })
+  }
+
+  return (
+    <div className='bg-black overflow-auto h-screen text-white p-4'>
+      <div className='h-[87%] flex flex-wrap gap-4 p-2'>
+        {printUserData}
+      </div>
+      <div className='flex justify-center gap-6 text-sm items-center p-4'>
+        <button
+         style={{opacity:index == 1?0.5:1}}
+         className='bg-amber-400 text-black rounded px-4 py-2 font-semibold cursor-pointer active:scale-95'
+         onClick={()=>{
+            if(index>1){
+              setIndex(index-1)
+              setUserData([])
+            }
+         }}
+        >
+          Prev
+        </button>
+        <h4>Page {index}</h4>
+        <button
+         className='bg-amber-400 text-black rounded px-4 py-2 font-semibold cursor-pointer active:scale-95'
+         onClick={()=>{
+            setIndex(index+1)
+            setUserData([])
+         }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
